cb3bb33721210ffe86c710557df8f1308abe1048
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-14 13:44:04 -0300

    Aula 01 - Introdução ao React

---
+++ 01_Fundamentals/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export function Home(){
+    
+    return(
+        <>
+            <h1>Hello World!</h1>
+            <span>Bruno Bandeira Fernandes</span>
+        </>
+    )
+}
+
