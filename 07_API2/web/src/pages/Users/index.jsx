dc2307e35b5c475ecd3574c7c80b06eeeab2f2ce
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-12-12 22:17:27 -0300

    users

---
+++ 07_API2/web/src/pages/Users/index.jsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
+import { Container, Header, Item } from "./styles";
+import { Button } from '../../components/Button';
+
+export function Users() {
+  const navigate = useNavigate();
+
+  const products = Array(20)
+    .fill({ name: 'User' })
+    .map((item, index) => (`${item.name} ${index + 1}`));
+
+  return (
+    <Container>
+      <Header>
+        <h1>Usuários</h1>
+
+        <nav>
+          <Button title="Voltar" onClick={() => navigate('/')} />
+        </nav>
+      </Header>
+
+      {
+        products.map((user) => (
+          <Item key={user}>
+            <span>{user}</span>
+          </Item>
+        ))
+      }
+    </Container>
+
+  )
+}
