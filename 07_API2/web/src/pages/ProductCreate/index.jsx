3d25cb851fa0d5e11347d9e804136a065bc0e6e0
brunobandeiraf <brunobandeirafernandes@gmail.com>
2024-08-21 13:10:48 -0300

    Atualização da aula 07

--- 07_API2/web/src/pages/ProductCreate/index.jsx
+++ 07_API2/web/src/pages/ProductCreate/index.jsx
@@ -1,10 +1,64 @@
-import { Container } from "./styles";
+import { useState } from "react";
+import { FiTag, FiPackage  } from 'react-icons/fi';
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function ProductCreate() {
+  
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleCreateProduct() {
+    if (!name || !price) {
+      return alert("Preencha todos os campos!");
+    }
+
+    api.post("/products", { name, price })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+        navigate("/");
+      }).catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar o produto.");
+        }
+      });
+  }
+
   return (
     <Container>
-      <h1>Criar Produto</h1>
-    </Container>
+    <Form>
+      <h1>Cadastro de Produto</h1>
+
+      <Input
+        placeholder="Nome"
+        type="text"
+        icon={FiPackage}
+        onChange={e => setName(e.target.value)}
+      />
+
+      <Input
+        placeholder="Preço"
+        type="text"
+        icon={FiTag}
+        onChange={e => setPrice(e.target.value)}
+      />
+
+      <Button
+        title="Cadastrar"
+        onClick={handleCreateProduct}
+      />
+    
+    </Form>
+  </Container>
 
   )
 }
--- 07_API2/web/src/pages/ProductCreate/styles.js
+++ 07_API2/web/src/pages/ProductCreate/styles.js
@@ -7,8 +7,23 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const Form = styled.form`
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+  padding: 54px 32px;
+  border-radius: 7px;
+
+  min-width: 370px;
+  
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 
   > h1 {
-    
+    font-size: 24px;
+    margin-bottom: 24px;
+    line-height: 32px;
   }
+  
 `;
